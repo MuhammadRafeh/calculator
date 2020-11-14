6772c4612cafa8f7bf66464943b0a56b1bf4b572
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2020-11-14 09:44:09 +0300

    Button Styling Done

--- components/Button.js
+++ components/Button.js
@@ -2,26 +2,48 @@ import React from 'react';
 import { 
 	TouchableOpacity,
 	StyleSheet,
+	Dimensions,
 	Text } from 'react-native';
 
-const Button = (props) => (
-	<TouchableOpacity 
-		onPress={props.onPress}
-		style={styles.button}>
-		<Text style={styles.text}>{props.text}</Text>
-	</TouchableOpacity>
-)
+const screen = Dimensions.get('window');
+const buttonWidth = screen.width / 4
+
+const Button = (props) => {
+	const buttonStyles = [styles.button];
+
+ 	if (props.size === 'double') {
+    	buttonStyles.push(styles.buttonDouble);
+ 	}
+
+ 	return(
+		<TouchableOpacity 
+			onPress={props.onPress}
+			style={buttonStyles}>
+			<Text style={styles.text}>{props.text}</Text>
+		</TouchableOpacity>
+	)
+}
 
 export default Button
 
 const styles = StyleSheet.create({
 	button: {
 		flex: 1,
-		backgroundColor: '#333333'
+		backgroundColor: '#333333',
+		height: Math.floor(buttonWidth - 10),
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: Math.floor(buttonWidth),
+		margin: 5
 	},
 	text: {
 		color: 'white',
 		fontSize: 25,
-		alignItems: 'center'
+	},
+	buttonDouble: {
+		flex: 0,
+		width: screen.width / 2 - 10,
+		alignItems: 'flex-start',
+		paddingLeft: 40,
 	}
 })
